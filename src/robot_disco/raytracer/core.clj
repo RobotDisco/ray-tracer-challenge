733db642e7e57fe9c1d06b5e3c43f2367a08161b
Gaelan D'costa <gdcosta@gmail.com>
2019-06-29 21:39:02 -0400

    Adding clojure specs
    Ignoring tuplewise functions for now as I realized I can do that in a
    more idiomatic way.

--- src/robot_disco/raytracer/core.clj
+++ src/robot_disco/raytracer/core.clj
@@ -1,55 +1,94 @@
-(ns robot-disco.raytracer.core)
+(ns robot-disco.raytracer.core
+  (:require [clojure.spec.alpha :as s]))
 
 (def ^:const EPSILON 0.00001)
+
+(s/def ::vector (s/tuple number? number? number? (s/and
+                                                  number?
+                                                  zero?)))
+(s/def ::point (s/tuple number? number? number? (s/and
+                                                 number?
+                                                 #(< (Math/abs (- 1.0 %)) EPSILON))))
+(s/def ::tuple (s/or :vector ::vector :point ::point))
 
 (defn make-tuple
   "Make a generic tuple"
   [x y z w]
   [(double x) (double y) (double z) (double w)])
 
+(s/fdef make-tuple
+  :args (s/cat :x number? :y number? :z number? :w number?)
+  :ret ::tuple)
+
 (defn make-point
   "Make a point tuple"
   [x y z]
   (make-tuple x y z 1.0))
+
+(s/fdef make-point
+  :args (s/cat :x number? :y number? :z number?)
+  :ret ::point)
 
 (defn make-vector
   "Make a vector tuple"
   [x y z]
   (make-tuple x y z 0.0))
 
+(s/fdef make-vector
+  :args (s/cat :x number? :y number? :z number?)
+  :ret ::vector)
+
 (defn x
   "tuple x coordinate"
   [t]
   (t 0))
+
+(s/fdef x
+  :args (s/cat :tuple ::tuple)
+  :ret number?)
 
 (defn y
   "tuple y coordinate"
   [t]
   (t 1))
 
+(s/fdef y
+  :args (s/cat :tuple ::tuple)
+  :ret number?)
+
 (defn z
   "tuple z coordinate"
   [t]
   (t 2))
+
+(s/fdef z
+  :args (s/cat :tuple ::tuple)
+  :ret number?)
 
 (defn w
   "tuple w/type coordinate"
   [t]
   (t 3))
 
-(defn point?
-  "Is this map a point?"
-  [t]
-  (let [w (w t)]
-    (and w
-         (== w 1.0))))
+(s/fdef w
+  :args (s/cat :tuple ::tuple)
+  :ret number?)
 
-(defn vector?
+(def point?
+  "Is this map a point?"
+  (partial s/valid? ::point))
+
+(s/fdef point?
+  :args (s/cat :tuple ::tuple)
+  :ret boolean?)
+
+(def vector?
   "Is this map a vector?"
-  [t]
-  (let [w (w t)]
-    (and w
-         (== w 0.0))))
+  (partial s/valid? ::vector))
+
+(s/fdef vector?
+  :args (s/cat :tuple ::tuple)
+  :ret boolean?)
 
 (defn tuplewise
   "Perform an operation on every entry of the tuple"
@@ -83,11 +122,20 @@
                              (Math/pow (z v) 2.0)
                              (Math/pow (w v) 2.0))))
 
+(s/fdef magnitude
+  :args (s/cat :vector ::vector)
+  :ret pos?)
+
 (defn normalize
   "Normalize vector"
   [v]
   (let [mag (magnitude v)]
     (/ v mag)))
+
+(s/fdef normalize
+  :args (s/cat :vector ::vector)
+  :ret ::vector
+  :fn #(not (zero? (magnitude %))))
 
 (defn =
   "Are two tuples close enough to another (every equivalent coord within epsilon)"
@@ -95,11 +143,19 @@
   (every? #(< % EPSILON)
           (map (comp #(Math/abs %) clojure.core/-) a b)))
 
+(s/fdef =
+  :args (s/tuple ::tuple ::tuple)
+  :ret boolean?)
+
 (defn dot
-  "Compute dot product of vectors"
-  [& vectors]
+  "Compute dot product of two vectors"
+  [v1 v2]
   (reduce clojure.core/+
-          (apply map clojure.core/* vectors)))
+          (map clojure.core/* v1 v2)))
+
+(s/fdef dot
+  :args (s/tuple ::vector ::vector)
+  :ret number?)
 
 (defn cross
   "Compute cross product of two vectors"
@@ -110,3 +166,7 @@
      (- (* ay bz) (* az by))
      (- (* az bx) (* ax bz))
      (- (* ax by) (* ay bx)))))
+
+(s/fdef cross
+  :args (s/tuple ::vector ::vector)
+  :ret ::vector)
--- src/robot_disco/raytracer/ticker.clj
+++ src/robot_disco/raytracer/ticker.clj
@@ -1,23 +1,59 @@
 (ns robot-disco.raytracer.ticker
-  (:require [robot-disco.raytracer.core :as rt]))
+  (:require [robot-disco.raytracer.core :as rt]
+            [clojure.spec.alpha :as s]))
+
+(s/def ::position :robot-disco.raytracer.core/point)
+(s/def ::velocity :robot-disco.raytracer.core/vector)
+(s/def ::projectile (s/keys :req-un [::position ::velocity]))
+
+(s/def ::gravity :robot-disco.raytracer.core/vector)
+(s/def ::wind :robot-disco.raytracer.core/vector)
+(s/def ::environment (s/keys :req-un [::gravity ::wind]))
 
 (defn make-projectile
   [pos vel]
   {:position pos
    :velocity vel})
 
+(s/fdef make-projectile
+  :args (s/tuple ::position ::velocity))
+
 (def position :position)
+
+(s/fdef position
+  :args (s/tuple ::projectile)
+  :ret ::position)
+
 (def velocity :velocity)
+
+(s/fdef velocity
+  :args (s/tuple ::projectile)
+  :ret ::velocity)
 
 (defn make-environment
   [gravity wind]
   {:gravity gravity
    :wind wind})
 
+(s/fdef make-environment
+  :args (s/tuple ::gravity ::wind)
+  :ret ::environment)
+
 (def gravity :gravity)
+(s/fdef gravity
+  :args (s/tuple ::environment)
+  :ret ::gravity)
+
 (def wind :wind)
+(s/fdef wind
+  :args (s/tuple ::environment)
+  :ret ::wind)
 
 (defn tick
   [env proj]
   {:position (rt/+ (position proj) (velocity proj))
    :velocity (rt/+ (velocity proj) (gravity env) (wind env))})
+
+(s/fdef tick
+  :args (s/tuple ::environment ::projectile)
+  :ret ::projectile)
